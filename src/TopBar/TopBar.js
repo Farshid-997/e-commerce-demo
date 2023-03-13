113617a15b484b74d63a9107a8fe0baf2ed5feb5
Farshid-997 <arnob.farshid@gmail.com>
2023-03-14 03:02:01 +0600

    added

--- src/TopBar/TopBar.js
+++ src/TopBar/TopBar.js
@@ -2,16 +2,22 @@ import React from "react";
 import logo from "../Assests/Vector 1.png";
 import "./TopBar.css";
 import line from "../Assests/Line 7.png";
+import mobile from "../Assests/XMLID_792_.png";
+import login from "../Assests/Vector.png";
+import love from "../Assests/Vector (1).png";
+
+import { FiShoppingCart } from "react-icons/fi";
+
 export default function TopBar() {
   return (
     <>
       {/* parent component of topbar start here */}
-      <div className="d-flex justify-between main-div">
+      <nav className="d-flex justify-content-between main-div container-fluid">
         {/* image section and input div start here  */}
 
-        <div className="d-flex">
+        <section className="d-flex">
           <div className="d-flex">
-            <img src={logo} alt="logo"></img>
+            <img src={logo} alt="logo" className="logo-img"></img>
 
             <h6 className="logo-text">store</h6>
           </div>
@@ -25,16 +31,43 @@ export default function TopBar() {
                 <option value="saab">All categories</option>
               </select>
 
-               {/* line-img */}
-            <img src={line} alt="input-line" className="line-img"></img>
+              {/* line-img */}
+              <img src={line} alt="input-line" className="line-img"></img>
             </div>
             {/* select div ends here */}
-
-           
           </div>
-        </div>
+        </section>
         {/* image section and input div ends here  */}
-      </div>
+
+        {/* 2nd section starts here */}
+
+        <section className="d-flex flex-row justify-content-between ">
+          {/* second section icons start */}
+          <div className="d-flex flex-column">
+            {/* first text */}
+
+            <p className="call-us">Call Us Now</p>
+
+            <div className="d-flex justify-content-center">
+              <img src={mobile} alt="mobile-icon" className="mobile-icon"></img>
+              <p className="mbl-number">+011 5827918</p>
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-evenly">
+            <img src={login} alt="" className="login-icon"></img>
+
+            <img src={love} alt="" className="love-icon"></img>
+
+            {/* cart icon and cat name */}
+            <div className="d-flex justify-content-end">
+              <FiShoppingCart className="cart-icon" />
+
+              <p className="cart-name">Cart</p>
+            </div>
+          </div>
+        </section>
+      </nav>
       {/* parent component of topbar ends here */}
     </>
   );
